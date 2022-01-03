9229a6464a325a43998b004fc14bf5e229c49a28
Ché John <chejohn16@gmail.com>
2022-01-02 21:57:22 -0500

    full implementation if priority queue

--- src/Libraries/PriorityQueue/PQ.test.js
+++ src/Libraries/PriorityQueue/PQ.test.js
@@ -2,18 +2,64 @@ import priorityQueue from './PriorityQueue';
 
 describe('priority queue library', () => {
     const pq = priorityQueue();
+    const pq2 = priorityQueue();
     
-    const pqNode1 = {node:'node1', distanceFromStart: 1};
+    const pqNode1 = { node: 'node1', distanceFromStart: 1 };
     const pqNode2 = { node: 'node2', distanceFromStart: 4 };
-    const pqNode3 = { node: 'node3', distanceFromStart: 4 };
-    const pqNode4 = { node: 'node4', distanceFromStart: 0};
-    const pqNode5 = { node: 'node5', distanceFromStart: 9};
+    const pqNode3 = { node: 'node3', distanceFromStart: 3 };
+    const pqNode4 = { node: 'node4', distanceFromStart: -1 };
+    const pqNode5 = { node: 'node5', distanceFromStart: 0 };
+    const pqNode6 = { node: 'node6', distanceFromStart: 9 };
+    const pqNode7 = { node: 'node7', distanceFromStart: -66 };
+    const pqNode8 = { node: 'node8', distanceFromStart: 1000 };
+
+    const pq2Node = { node: 'node8', distanceFromStart: 5000 };
+    pq2.insert(pq2Node);
+
     pq.insert(pqNode1);
     pq.insert(pqNode2);
     pq.insert(pqNode3);
     pq.insert(pqNode4);
     pq.insert(pqNode5);
-    test('insert method', () => {
-      expect(pq.rep[0]).toEqual({ node: 'node4', distanceFromStart: 0});
+    pq.insert(pqNode6);
+    pq.insert(pqNode7);
+    pq.insert(pqNode8);
+    test('poll method', () => {
+      expect(pq.poll()).toEqual({ node: 'node7', distanceFromStart: -66});
+      expect(pq.poll()).toEqual({ node: 'node4', distanceFromStart: -1});
+      expect(pq.poll()).toEqual({ node: 'node5', distanceFromStart: 0 });
+      expect(pq.poll()).toEqual({ node: 'node1', distanceFromStart: 1 });
+      expect(pq.poll()).toEqual({ node: 'node3', distanceFromStart: 3 });
+      expect(pq.poll()).toEqual({ node: 'node2', distanceFromStart: 4 });
+      expect(pq.poll()).toEqual({ node: 'node6', distanceFromStart: 9 });
+      expect(pq.poll()).toEqual({ node: 'node8', distanceFromStart: 1000});
+      
+      expect(pq2.poll()).toEqual({ node: 'node8', distanceFromStart: 5000 });
+    });
+    test('update priority queue method', () => {
+      pq.insert(pqNode1);
+      pq.insert(pqNode2);
+      pq.insert(pqNode3);
+      pq.insert(pqNode4);
+      pq.insert(pqNode5);
+      pq.insert(pqNode6);
+      pq.insert(pqNode7);
+      pq.insert(pqNode8);
+      pq.updatePQNode('node8', -200);
+      pq.updatePQNode('node7', -150);
+      pq.updatePQNode('node6', -100);
+      pq.updatePQNode('node5', -50);
+      pq.updatePQNode('node4', -10);
+      pq.updatePQNode('node3', 70);
+      pq.updatePQNode('node2', 80);
+      pq.updatePQNode('node1', 90);
+      expect(pq.poll()).toEqual({ node: 'node8', distanceFromStart: -200 });
+      expect(pq.poll()).toEqual({ node: 'node7', distanceFromStart: -150 });
+      expect(pq.poll()).toEqual({ node: 'node6', distanceFromStart: -100 });
+      expect(pq.poll()).toEqual({ node: 'node5', distanceFromStart: -50});
+      expect(pq.poll()).toEqual({ node: 'node4', distanceFromStart: -10});
+      expect(pq.poll()).toEqual({ node: 'node3', distanceFromStart: 70});
+      expect(pq.poll()).toEqual({ node: 'node2', distanceFromStart: 80});
+      expect(pq.poll()).toEqual({ node: 'node1', distanceFromStart: 90});
     });
 });
--- src/Libraries/PriorityQueue/PriorityQueue.js
+++ src/Libraries/PriorityQueue/PriorityQueue.js
@@ -5,6 +5,32 @@ const priorityQueue = () => {
     const rep = [];
     let pqSize = 0;
 
+    const sink = (parentIndex) => {
+        while (true) {
+          const leftIndex = 2 * parentIndex + 1;
+          const rightIndex = 2 * parentIndex + 2;
+          let smallest = leftIndex;
+
+          if (rightIndex < pqSize && less(rightIndex, leftIndex))
+            smallest = rightIndex;
+
+          if (leftIndex >= pqSize || less(parentIndex, smallest)) break;
+
+          swap(parentIndex, smallest);
+          parentIndex = smallest; // traversing down the tree
+        }
+    }
+
+    const swim = (childIndex) => {
+        let parentIndex = Math.floor((childIndex - 1) / 2);
+
+        while (childIndex > 0 && less(childIndex, parentIndex)) {
+          swap(childIndex, parentIndex);
+          childIndex = parentIndex; // traversing up the tree
+          parentIndex = Math.floor((childIndex - 1) / 2);
+        }
+    }
+
     const less = (index1, index2) => {
         if (rep[index1].distanceFromStart < rep[index2].distanceFromStart) return true;
         return false;
@@ -23,44 +49,44 @@ const priorityQueue = () => {
     const insert = (pqNode) => {
         rep.push(pqNode);
         pqSize++;
-        let childIndex = rep.length-1;
-        let parentIndex = Math.floor((childIndex-1)/2); 
-        
-        // swim operation
-        while (childIndex > 0 && less(childIndex, parentIndex)) {
-            swap(childIndex, parentIndex);
-            childIndex = parentIndex; // traversing up the tree
-            parentIndex = Math.floor((childIndex - 1) / 2);
-        }
+        let childIndex = rep.length - 1;
+        swim(childIndex);
     }
 
     /*
     params: void
     returns: priorityQueue node
+    function: removes root node from priority queue
     */
     const poll = () => {
         swap(0, rep.length-1);
         const smallestNode = rep.pop();
         pqSize--;
         let parentIndex = 0;
-        
-        // sink operation
-        while(true) {
-            const leftIndex = 2*parentIndex+1;
-            const rightIndex = 2*parentIndex+2;
-            let smallest = leftIndex;
-
-            if (rightIndex < pqSize && less(rightIndex, leftIndex)) smallest = rightIndex;
-            
-            if (leftIndex >= pqSize || less(parentIndex, smallest)) break;
-
-            swap(parentIndex, smallest);
-            parentIndex = smallest; // traversing down the tree
-        }
+        sink(parentIndex);
         return smallestNode;
     }
+    
+    /*
+    params: node from graph; integer distance of that node from starting point
+    returns: void
+    */
+    const updatePQNode = (graphNode, distanceFromOrigin) => {
+        for (let i = 0; i < pqSize; i++) {
+            if (JSON.stringify(rep[i].node) === JSON.stringify(graphNode)) {
+                rep[i].distanceFromStart = distanceFromOrigin;
+                const parentIndex = Math.floor((i-1)/2);
+                const leftIndex = 2*i + 1;
+                const rightIndex = 2*i + 2;
+                if (parentIndex >= 0 && less(i, parentIndex)) swim(i);
+                else if ((leftIndex < pqSize && less(leftIndex, i))|| 
+                (rightIndex < pqSize && less(rightIndex, i))) sink(i);
+                break;
+            }
+        }
+    }
 
-    return {insert, poll, rep}
+    return {insert, poll, updatePQNode,rep}
 }
 
 export default priorityQueue;
